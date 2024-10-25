aeec631b6a5cb835af16351d65b0879138768aee
Andas-LV <andassatybaldy@gmail.com>
2024-10-25 19:02:52 +0500

    no aws 2

--- cookly/settings.py
+++ cookly/settings.py
@@ -142,18 +142,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-# AWS settings
-# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
 
-# AWS_S3_FILE_OVERWRITE = True
-# AWS_DEFAULT_ACL = 'public-read'
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 
