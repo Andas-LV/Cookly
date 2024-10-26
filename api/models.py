2b2e0a5c7e3c747c2e6ee410dff3dc2b6965b6de
Andas-LV <andassatybaldy@gmail.com>
2024-10-26 10:08:53 +0500

    again aws

--- api/models.py
+++ api/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.files.storage import DefaultStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, storage=DefaultStorage())
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, storage=S3Boto3Storage())
 
     def __str__(self):
         return self.user.username
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='products/', null=True, blank=True, storage=DefaultStorage())
+    image = models.ImageField(upload_to='products/', null=True, blank=True, storage=S3Boto3Storage())
 
     class Meta:
         db_table = 'products'
@@ -23,7 +23,7 @@ class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     products = models.ManyToManyField(Product, related_name='recipes')
-    image = models.ImageField(upload_to='recipes/', null=True, blank=True, storage=DefaultStorage())
+    image = models.ImageField(upload_to='recipes/', null=True, blank=True, storage=S3Boto3Storage())
 
     class Meta:
         db_table = 'recipes'
--- cookly/settings.py
+++ cookly/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'api',
+    'storages',
     'corsheaders',
     'drf_yasg',
 ]
